f5fc8c066dacf0e0f828ad1bc14d2cd43e6ec44e
comrade369 <saveallchildreninworld@gmail.com>
2023-11-04 16:04:15 +0530

    About Components in ReactJS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import FunctionalComponent from "./Components/FunctionalComponent";
+import {ClassComponent, AnotherClassComponent} from "./Components/ClassComponent";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>Hello, Welcome to application-one</h1>
+    <h1>This is about Components in ReactJS</h1>
+    <FunctionalComponent />
+    <ClassComponent />
+    <AnotherClassComponent />
     </div>
   );
 }
---
+++ src/Components/FunctionalComponent.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function FunctionalComponent() {
+    return (
+        <p>This is a Functional Component.</p>
+    );
+}
+
+export default FunctionalComponent; // we can rename component while importing benift of default export. 
